c18952347860338196097b412d2115f2625c46b3
Chong <chong.w.lau@gmail.com>
2023-01-25 22:26:23 +0000

    dashboard FE loads, need to write tests and get BE working

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.db.models import Count, Q, Sum, F
+
+from .models import Quotes
+
 
 # Register your models here.
+@admin.register(Quotes)
+class DashboardViewAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/quotes_change_list.html'
+    
+    def changelist_view(self, request, extra_content=None):
+        response = super().changelist_view(
+            request, extra_context=extra_content
+        )
+        
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+        
+        summary = {
+            'sales_count': Count('sale_indicator', filter=Q(sale_indicator__exact=1)),
+            'non_sales_count': Count('sale_indicator', filter=Q(sale_indicator__exact=0)),
+            'total_price_sum': Sum('total_price'),
+            'net_price_sum': Sum('net_price'),
+            'gross_price_sum': F('total_price_sum') - F('net_price_sum')
+        }
+        
+        response.context_data['quote_summary_total'] = \
+            qs.values('test_group').annotate(**summary).order_by()
+
+        return response
--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from dashboard.models import Quotes
+from dashboard.admin import DashboardViewAdmin
 
 # Create your tests here.
+class DashboardTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        Quotes.objects.create()
